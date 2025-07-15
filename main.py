9f7b64ff85fd038537eeed181780e4b602e06974
imoT-py <imot.nagy@gmail.com>
2025-07-15 15:00:12 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+from puuids import get_puuids_challengers
+from puuids import get_puuids_grandmasters
+from puuids import get_puuids_masters
+from puuids import get_puuids
+from process import process
+from create_folders import folders
+from user_rank import user_rank
+
+# Create folders based on ranks
+folders()
+
+#Get the user_IDs in a list | Below DIAMOND
+puuids, rank = get_puuids()
+process(puuids, 30, rank)
+
+#Get the Challengers
+puuids = get_puuids_challengers()
+process(puuids, 100, 'challenger')
+
+#Get the Grandmasters
+puuids = get_puuids_grandmasters()
+process(puuids, 100, 'grandmaster')
+
+#Get the Masters
+puuids = get_puuids_masters()
+process(puuids, 100, 'master')
+
+#Create user_rank folder with all users and their ranks
+user_rank()
