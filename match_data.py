28ee652be1726fb58871aa91db1ca2be8990198d
imoT-py <imot.nagy@gmail.com>
2025-07-15 15:20:34 +0200

    Add match_data.py

---
+++ match_data.py
@@ -0,0 +1,20 @@
+import requests
+from api import headers
+import time
+
+def is_ranked(match_ID):
+  
+  while True:
+    response = requests.get(f"https://europe.api.riotgames.com/lol/match/v5/matches/{match_ID}", headers=headers) # 2000 requests every 10 seconds
+    if response.status_code == 200:
+      break
+    else:
+      print("Waiting for API")
+      time.sleep(20)
+      continue
+
+  data = response.json()
+  ranked_info = data['info']['queueId']
+
+  return ranked_info
+
